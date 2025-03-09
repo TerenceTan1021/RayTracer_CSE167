92cdcb667065055c8cb463bccd80eb43afe15b98
Terence Tan <tetan1021@gmail.com>
2025-03-09 16:48:31 -0700

    Implemented the sphere(not yet done) and added some information to the README write up file

--- README.md
+++ README.md
@@ -2,7 +2,45 @@ RayTracer Program(CSE 167, WINTER 2025)
 
 RayTracer.cpp
 
-Function Ray RayTracer::ray_thru_pixel()
+Function: vector<Intersection> GeomSphere::ntersect(Ray &ray)
+variables:
+- c: sphere center
+- r: sphere radius
+- p_0: ray Source
+- d: ray direction
+- 
+this funtion follow the sphere intersection quadratic equation for finding the sphere intersect:
+
+||r_o + tr_d - c||^2 = R^2
+
+to find t, we need to find where the point lies on the sphere, the point only lies on the sphere if and only if: (q - c) * (q -c) = r^2
+
+subsituting the formulas gives us:
+
+(p_0 + td - c) * (p_0 + td - c) = r^2
+
+Expand:
+
+|d|^2t^2 + 2d * (p_0 - c)t + |p_0 = c|^2 - r^2 = 0,
+where |d|^2 = 1, because of normalization
+
+t^2 + 2d * (p_0 - c)t + |p_0 = c|^2 - r^2 = 0
+
+which gives us:
+
+t = -d * (p_0 - c) +- sqrt((d * (p_0 - c))^2 - |p_0 - c|^2 + r^2)
+
+where if the expression within the sqrt is
+  - negative: there is no intersection
+  - 0 : there is only one intersection(tangent)
+  - positive: there are two instersection, and we put plus in the smallest positive t value.
+
+after finding the intersection, we would then find the point posiiton and the Normal:
+
+Point position: q = p_0 + td
+Normal: normalize(q - c)
+
+
 
 
 
--- src/geometries/GeomSphere.cpp
+++ src/geometries/GeomSphere.cpp
@@ -37,6 +37,65 @@ std::vector<Intersection> GeomSphere::intersect(Ray &ray) {
      *   where t > 0.
      */
 
+    //Sphere center and radius
+    vec3 c = this->center;
+    float r = this->radius;
+
+    //ray dir and Source
+    vec3 d = ray.dir;
+    vec3 p_0 = ray.p0;
+
+    //t formula broken into 3 parts
+    vec3 pc = p_0 - c;
+    vec3 neg_d = d - d - d;
+    float tempA = dot(neg_d, pc);
+
+    float tempB = dot(d, pc) * dot(d, pc);
+    vec3 absolute = abs(pc);
+    float tempC =  dot(absolute, absolute);
+    float tempD = pow(r, 2);
+    //the section inside the sqrt 
+    float section = tempB - tempC + tempD;
+
+    //if the expression in sqrt is negative --> no intersection
+    if(section < 0.0f){
+        return intersections;
+    }
+     /*
+    if the expression in sqrt is positive --> two intersections
+                                          --> take smallest pos t
+    */
+    else if(section > 0.0f){
+        //calculate the two t values
+        float sqrt_section = sqrt(section);
+
+        float t1 = tempA + sqrt_section;
+        float t2 = tempA - sqrt_section;
+
+        //take in the smalest positve t
+        if((t1 < t2) && (t1 >= 0.0f)){
+            vec3 q = p_0 + t1 * d;
+            vec3 n = normalize(q - c);
+            intersections.push_back({t1, q, n, this, nullptr});
+        }
+        else if((t2 < t1) && (t2 >= 0.0f)){
+            vec3 q = p_0 + t2 * d;
+            vec3 n = normalize(q - c);
+            intersections.push_back({t2, q, n, this, nullptr});
+        }
+    }
+    //if the expression in sqrt is a zero --> tangent
+    else if(section == 0.0f){
+        float sqrt_section = sqrt(section);
+        float t = tempA + sqrt_section;
+        if(t >= 0){
+            vec3 q = p_0 + t * d;
+            vec3 n = normalize(q - c);
+            intersections.push_back({t, q, n, this, nullptr});
+        }
+
+    }
+
     /**
      * TODO: Update `intersections`
      */
